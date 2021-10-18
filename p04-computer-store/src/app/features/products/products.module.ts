1ab32b242269d8409a8958231153b7be4f95fb10
Mariana <marianaufc@gmail.com>
2021-10-18 10:50:28 -0300

    feat(p04): creates basic structure

---
+++ p04-computer-store/src/app/features/products/products.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductCardComponent,
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProductListComponent
+  ]
+})
+export class ProductsModule { }
