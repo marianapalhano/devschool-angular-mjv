dd0c97449f02c130832799562b33241fedd39080
Mariana <marianaufc@gmail.com>
2021-11-04 15:10:56 -0300

    feat(p06): uses AuthGuard to protect routes

--- p06-gym-manager/src/app/app-routing.module.ts
+++ p06-gym-manager/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './features/login/pages/login/login.component';
 import { RegisterComponent } from './features/login/pages/register/register.component';
 import { MemberDetailsComponent } from './features/members/pages/member-details/member-details.component';
 import { MembersComponent } from './features/members/pages/members/members.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'members', component: MembersComponent },
-  { path: 'member-details/:id', component: MemberDetailsComponent },
+  { path: 'members', component: MembersComponent, canActivate: [AuthGuard] },
+  { path: 'member-details/:id', component: MemberDetailsComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent }
 ];
 
