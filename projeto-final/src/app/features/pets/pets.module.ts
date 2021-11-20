a35851f31287e8a26233ce7d99caa89c1e09dd93
Mariana <marianaufc@gmail.com>
2021-11-20 16:25:58 -0300

    feat(projeto-final): creates home page

--- projeto-final/src/app/features/pets/pets.module.ts
+++ projeto-final/src/app/features/pets/pets.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { PetsComponent } from './pages/pets/pets.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PetDetailsComponent } from './pages/pet-details/pet-details.component';
+import { PetComponent } from './components/pet/pet.component';
+import { PetListComponent } from './components/pet-list/pet-list.component';
 
 
 
 @NgModule({
   declarations: [
     PetsComponent,
-    PetDetailsComponent
+    PetDetailsComponent,
+    PetComponent,
+    PetListComponent
   ],
   imports: [
     CommonModule,
--- projeto-final/src/app/shared/shared.module.ts
+++ projeto-final/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -7,12 +8,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
 
 const materialModules = [
   MatToolbarModule,
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule
 ];
 
 const components = [
