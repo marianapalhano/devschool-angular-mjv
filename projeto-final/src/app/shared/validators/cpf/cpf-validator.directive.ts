c853670c20efeea127a54bc96e252c9ec76b7a76
Mariana <marianaufc@gmail.com>
2021-11-28 21:11:42 -0300

    fix(projeto-final): fixes cpf validator bug

--- projeto-final/src/app/shared/validators/cpf/cpf-validator.directive.ts
+++ projeto-final/src/app/shared/validators/cpf/cpf-validator.directive.ts
@@ -1,12 +1,29 @@
 import { Directive } from '@angular/core';
 import { AbstractControl, Validator, NG_VALIDATORS, ValidatorFn, ValidationErrors } from '@angular/forms';
 
+function testsCpf(cpf: string) {
+  let sum: number = 0;
+  let mod: number;
+
+  for (let i=1; i<=9; i++) sum = sum + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  mod = (sum * 10) % 11;
+
+    if ((mod == 10) || (mod == 11))  mod = 0;
+    if (mod != parseInt(cpf.substring(9, 10)) ) return false;
+
+  sum = 0;
+    for (let i = 1; i <= 10; i++) sum = sum + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    mod = (sum * 10) % 11;
+
+    if ((mod == 10) || (mod == 11))  mod = 0;
+    if (mod != parseInt(cpf.substring(10, 11) ) ) return false;
+    return true;
+}
 
 export function validatesCpf(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     let cpf = control.value.toString();
-    let sum: number = 0;
-    let mod: number;
+
     let valid: boolean;
   
     const regex = new RegExp('[0-9]{11}');
@@ -27,22 +44,25 @@ export function validatesCpf(): ValidatorFn {
       valid = false;
     }    
     else {
-      for (let i = 1; i <= 9; i++)
-        sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      mod = (sum * 10) % 11;
+      // for (let i = 1; i <= 9; i++)
+      //   sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      // mod = (sum * 10) % 11;
   
-      if (mod == 10 || mod == 11) mod = 0;
-      if (mod != parseInt(cpf.substring(9, 10))) valid = false;
+      // if (mod == 10 || mod == 11) mod = 0;
+      // if (mod != parseInt(cpf.substring(9, 10))) valid = false;
   
-      sum = 0;
-      for (let i = 1; i <= 10; i++)
-        sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      mod = (sum * 10) % 11;
+      // sum = 0;
+      // for (let i = 1; i <= 10; i++)
+      //   sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      // mod = (sum * 10) % 11;
   
-      if (mod == 10 || mod == 11) mod = 0;
-      if (mod != parseInt(cpf.substring(10, 11))) valid = false;
-      valid = true;
+      // if (mod == 10 || mod == 11) mod = 0;
+      // if (mod != parseInt(cpf.substring(10, 11))) valid = false;
+      // valid = true;
+      valid = testsCpf(cpf);
+
     }
+    console.log('aki ', valid)
     return !valid ? { cpf: {value: control.value}} : null;
   }
 }
